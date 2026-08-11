9630dd4321681f772889ec7589529d5880eada55
shoaibrasool <shooaib6@gmail.com>
2026-08-11 15:37:04 +0500

    Initial project structure with TS and health checkpoint.

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, {Request , Response} from 'express'
+
+const app = express()
+
+app.get('/health', (req : Request, res: Response)=>{
+    res.status(200).json({
+        status: "UP"
+    })
+})
+
+module.exports = app
