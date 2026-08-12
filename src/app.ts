8dde3e8a82d9b9a7379f4cf3943052e389b65ec1
shoaibrasool <shooaib6@gmail.com>
2026-08-12 18:47:24 +0500

    url verified and initial books fetched

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,32 @@
-import express, {Request , Response} from 'express'
+import axios from 'axios'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
-app.get('/health', (req : Request, res: Response)=>{
+app.get('/health', (req: Request, res: Response) => {
     res.status(200).json({
         status: "UP"
     })
+})
+
+app.get('/get-books', async (req: Request, res: Response) => {
+    try {
+        const limit = 10
+        const openLibraryUrl = `https://openlibrary.org/search.json?q=fiction&limit=${limit}`;
+
+        const response = await axios.get(openLibraryUrl)
+        const books = response.data?.docs || [];
+
+        return res.status(200).json({
+            success: true,
+            data: books,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed To Fetch Books",
+        });
+    }
 })
 
 module.exports = app
