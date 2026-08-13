b6e74b5cd64ff6e0f1b49f84c19d68bef3083898
shoaibrasool <shooaib6@gmail.com>
2026-08-13 15:07:40 +0500

    added sort on rating

--- backend/src/services/openLibraryService.ts
+++ backend/src/services/openLibraryService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchBooksFromOL = async () => {
     const limit = process.env.MAX_BOOKS || 10
-    const openLibraryUrl = `https://openlibrary.org/search.json?q=has_fulltext:true&limit=${limit}`;
+    const openLibraryUrl = `https://openlibrary.org/search.json?q=has_fulltext:true&sort=rating&limit=${limit}&fields=key,title,author_name,first_publish_year,cover_i,isbn`;
 
     const response = await axios.get(openLibraryUrl)
     const books = response.data?.docs || [];
