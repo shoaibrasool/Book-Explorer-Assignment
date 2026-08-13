ed4c66d2741fc79f096d3a96f1d61e73efa3f0b9
shoaibrasool <shooaib6@gmail.com>
2026-08-13 15:23:23 +0500

    bug fixes

--- backend/src/services/openLibraryService.ts
+++ backend/src/services/openLibraryService.ts
@@ -2,10 +2,13 @@ import axios from "axios";
 
 export const fetchBooksFromOL = async () => {
     const limit = process.env.MAX_BOOKS || 10
-    const openLibraryUrl = `https://openlibrary.org/search.json?q=has_fulltext:true&sort=rating&limit=${limit}&fields=key,title,author_name,first_publish_year,cover_i,isbn`;
+    const openLibraryUrl = `https://openlibrary.org/search.json?q=has_fulltext:true&sort=rating&language=eng&limit=${limit}&fields=key,title,author_name,first_publish_year,cover_i,isbn`;
 
-    const response = await axios.get(openLibraryUrl)
-    const books = response.data?.docs || [];
+    try {
+        const response = await axios.get(openLibraryUrl)
+        return response.data?.docs || []
+    } catch (error) {
+        throw new Error("Could not fetch books from Open Library")
+    }
 
-    return books
 }
